990dcd5105b79f720039758082efc2de64ccd23d
Jake Spurlock <whyisjake@gmail.com>
2021-05-11 16:41:46 -0700

    Better format the functions

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -3,14 +3,26 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 function geoFindMe() {
 
 	const status = document.querySelector('#status');
+	let latitude, longitude = '';
 
 	function success(position) {
+
+		// Let's add these to the cache.
+		localStorage.setItem('latitude', position.coords.latitude);
+		localStorage.setItem('longitude', position.coords.longitude);
+
 		const latitude  = position.coords.latitude;
 		const longitude = position.coords.longitude;
 
-		console.log(latitude, longitude);
-		console.log(getWeatherApiUrl(latitude, longitude));
 		status.textContent = `Getting weather for ${latitude},${longitude}`;
+		getWeather(latitude, longitude);
+	}
+
+	function error() {
+		status.textContent = 'Unable to retrieve your location';
+	}
+
+	function getWeather(latitude, longitude) {
 		fetch(getWeatherApiUrl(latitude, longitude))
 			.then(response => response.json())
 			.then(function(data) {
@@ -24,28 +36,35 @@ function geoFindMe() {
 			});
 	}
 
-	function error() {
-	  status.textContent = 'Unable to retrieve your location';
+	function getWeatherApiUrl(latitude, longitude) {
+		const params = {
+			appid: '3238e8f65f973df777e476458fb6780f',
+			lat: latitude,
+			lon: longitude,
+			units: 'imperial'
+		}
+		const paramString = new URLSearchParams(params).toString();
+		return `https://api.openweathermap.org/data/2.5/weather?${paramString}`;
 	}
+
 
 	if(!navigator.geolocation) {
-	  status.textContent = 'Geolocation is not supported by your browser';
+		status.textContent = 'Geolocation is not supported by your browser';
 	} else {
-	  status.textContent = 'Locating…';
-	  navigator.geolocation.getCurrentPosition(success, error);
+
+		// Do we have lat/long in the browser cache?
+		latitude = localStorage.latitude;
+		longitude = localStorage.longitude;
+
+		// We have it in the cache, let's just get the weather.
+		if (latitude !== undefined && longitude !== undefined) {
+			getWeather(latitude, longitude)
+		} else {
+			status.textContent = 'Locating…';
+			navigator.geolocation.getCurrentPosition(success, error);
+		}
 	}
 
-}
-
-function getWeatherApiUrl(latitude, longitude) {
-	const params = {
-		lat: latitude,
-		lon: longitude,
-		appid: '3238e8f65f973df777e476458fb6780f',
-		units: 'imperial'
-	}
-	const paramString = new URLSearchParams(params).toString();
-	return `https://api.openweathermap.org/data/2.5/weather?${paramString}`;
 }
 
 document.addEventListener("DOMContentLoaded", geoFindMe);
