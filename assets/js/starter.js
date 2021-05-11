b6c81adc58f9df65218510774afe070dacfa1a99
Jake Spurlock <whyisjake@gmail.com>
2021-05-11 15:22:48 -0700

    Make this all a little simpler.

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -1,28 +1,4 @@
-// Importing JavaScript
-//
-// You have two choices for including Bootstrap's JS files—the whole thing,
-// or just the bits that you need.
-
-
-// Option 1
-//
-// Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
-
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
-// Option 2
-//
-// Import just what we need
-
-// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
-// import "../../node_modules/popper.js/dist/popper.min.js";
-
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
-
-
-console.log('Jake was here');
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function geoFindMe() {
 
@@ -39,11 +15,11 @@ function geoFindMe() {
 			.then(response => response.json())
 			.then(function(data) {
 				console.log(data);
-				status.innerHTML = `Temp: ${data.main.feels_like}<br>`;
+				status.innerHTML = `Temp: ${data.main.feels_like}º<br>`;
 				if (data.main.feels_like > 67) {
-					status.innerHTML += '<br>Yeah, prolly not sweater weather';
+					status.innerHTML += 'Yeah, prolly not sweater weather';
 				} else {
-					status.textContent += '<br>Looks like sweater weather.'
+					status.textContent += 'Looks like sweater weather.'
 				}
 			});
 	}
@@ -62,7 +38,14 @@ function geoFindMe() {
 }
 
 function getWeatherApiUrl(latitude, longitude) {
-	return `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=3238e8f65f973df777e476458fb6780f&units=imperial`;
+	const params = {
+		lat: latitude,
+		lon: longitude,
+		appid: '3238e8f65f973df777e476458fb6780f',
+		units: 'imperial'
+	}
+	const paramString = new URLSearchParams(params).toString();
+	return `https://api.openweathermap.org/data/2.5/weather?${paramString}`;
 }
 
 document.addEventListener("DOMContentLoaded", geoFindMe);
